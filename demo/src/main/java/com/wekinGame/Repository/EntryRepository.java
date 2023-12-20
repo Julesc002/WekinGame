899074642930af88ef7222790c6ae327779506b1
paria12 <enzo12850@gmail.com>
2023-12-20 17:40:17 +0100

    Refactoring categoryController

--- demo/src/main/java/com/wekinGame/Repository/EntryRepository.java
+++ demo/src/main/java/com/wekinGame/Repository/EntryRepository.java
@@ -1,21 +1,28 @@
 package com.wekinGame.Repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.result.UpdateResult;
 
 public class EntryRepository {
 
-    private MongoDatabase database = DatabaseRepository.getDatabase();
-    private MongoCollection<Document> collection = database.getCollection("entrees");
+    private static MongoDatabase database = DatabaseRepository.getDatabase();
+    private static MongoCollection<Document> collection = database.getCollection("entrees");
 
-    public List<Document> getEntriesByIdWiki(int id){
+    public static List<Document> getEntriesByIdWiki(int id){
         Document searchQuery = new Document();
         searchQuery.put("id_wiki", id);
         List<Document> entries = new ArrayList<>();
@@ -28,4 +35,49 @@ public class EntryRepository {
         return entries;
     }
 
+    public static List<Document> getEntryByWikiAndCategory(int id_wiki, String nameCategory){
+        List<Document> entry = new ArrayList<Document>();
+            Document searchQuery = new Document();
+            searchQuery.put("id_wiki", id_wiki);
+            searchQuery.put("categories", nameCategory);
+            List<Bson> pipeline = Arrays.asList(
+                    Aggregates.match(searchQuery),
+                    Aggregates.project(Projections.fields(
+                    Projections.include("_id", "nom"))));
+            AggregateIterable<Document> cursor = collection.aggregate(pipeline);
+            try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+                while (cursorIterator.hasNext()) {
+                    entry.add(cursorIterator.next());
+                }
+            }
+            return entry;
+    }
+
+    public static void removeCategoryFromWikiEntries(Integer idWiki, String category) {
+        Document searchQuery = new Document();
+        searchQuery.put("id_wiki", idWiki);
+        searchQuery.put("categories", category);
+        Document updateQuery = new Document("$pull", new Document("categories", category));
+        collection.updateMany(searchQuery, updateQuery);
+    }
+
+    public static void removeEntriesWithNoCategories() {
+        collection.deleteMany(new Document("categories", new Document("$size", 0)));
+    }
+
+    public static String modifyCategoriesNameForEntries(String oldStringCategory,int idWiki, Document setQuery){
+    try{
+        Document searchQuery = new Document("$and", Arrays.asList(
+            Filters.eq("id_wiki", idWiki),
+            Filters.eq("categories", oldStringCategory))); 
+        UpdateResult result = collection.updateMany(searchQuery,setQuery);
+        if (result.getModifiedCount() == 0) {
+            return "404";
+        }
+            return "200";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "500";
+        }
+    }
 }
--- demo/src/main/java/com/wekinGame/Repository/WikiRepository.java
+++ demo/src/main/java/com/wekinGame/Repository/WikiRepository.java
@@ -1,27 +1,35 @@
 package com.wekinGame.Repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bson.Document;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.UpdateResult;
 
 public class WikiRepository {
 
-    private MongoDatabase database = DatabaseRepository.getDatabase();
-    private MongoCollection<Document> collection = database.getCollection("wikis");
+    private static MongoDatabase database = DatabaseRepository.getDatabase();
+    private static MongoCollection<Document> collection = database.getCollection("wikis");
 
-    public Document getWikiById(int id) {
+    public static Document getWikiById(int id) {
         Document searchQuery = new Document();
         searchQuery.put("_id", id);
         return collection.find(searchQuery).first();
     }
 
-    public List<Document> getWikisByNamePrefix(String prefix) {
+    public static List<Document> getWikisByNamePrefix(String prefix) {
         Document searchQuery = new Document();
         searchQuery.put("nom", new Document("$regex", prefix).append("$options", "i"));
         List<Document> wikis = new ArrayList<>();
@@ -34,4 +42,62 @@ public class WikiRepository {
         return wikis;
     }
 
+    public static Map<String,String> addCategory(int id,Map<String,String> request){
+        Document wiki = WikiRepository.getWikiById(id);
+        Document searchQuery = new Document();
+        searchQuery.put("_id", id);
+        String newCategory = request.get("nom");
+        List<String> categories = (List<String>) wiki.get("categories");
+        Map<String, String> response = new HashMap<>();
+        if (!categories.contains(newCategory)) {
+            categories.add(newCategory);
+            wiki.put("categories", categories);
+            collection.replaceOne(searchQuery, wiki);
+            response.put("code", "200");
+            response.put("message", "Catégorie ajoutée avec succès");
+        } else {
+            response.put("code", "409");
+            response.put("message", "La catégorie existe déjà");
+        }
+        return response;
+    }
+
+    public static void getDeleteCategory(int idWiki, String nameCategory){
+        collection.updateOne(Filters.eq("_id", idWiki), Updates.pull("categories", nameCategory));
+        EntryRepository.removeCategoryFromWikiEntries(idWiki, nameCategory);
+        EntryRepository.removeEntriesWithNoCategories();
+    }
+
+    public static String modifyCategoryNameForWikis(String oldStringCategory, int idWiki, Document setQuery) {
+        try {
+            Document searchQuery = new Document("$and", Arrays.asList(
+            Filters.eq("_id", idWiki),
+            Filters.eq("categories", oldStringCategory)));
+            UpdateResult result = collection.updateOne(searchQuery,setQuery);
+            if (result.getModifiedCount() == 0) {
+                return "404";
+            }
+            return "200";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "500";
+        }
+    }
+
+    public static ResponseEntity<String> ModifyCategoryName(Map<String, Object> oldStringCategory,String newCategory){
+        Document setQuery = new Document("$set", new Document("categories.$", newCategory));
+        String resultWikis = WikiRepository.modifyCategoryNameForWikis(
+            (String) oldStringCategory.get("categories"),
+            (Integer) oldStringCategory.get("id"), setQuery);
+        String resultEntries = EntryRepository.modifyCategoriesNameForEntries(
+            (String) oldStringCategory.get("categories"),
+            (Integer) oldStringCategory.get("id"), setQuery);
+        if (resultEntries.equals("404") && resultWikis.equals("404")) {
+            return new ResponseEntity<>("404 Not Found", HttpStatus.NOT_FOUND);
+        } else if (resultEntries.equals("200") && resultWikis.equals("200")) {
+            return new ResponseEntity<>("200 OK", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("500 Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
